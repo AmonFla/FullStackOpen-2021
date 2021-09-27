68fbe0172456e40c8929b920b89935faac62a370
Flavio Garrido <fbonnin@teceng-gaming.com.ar>
2021-09-27 12:03:20 -0300

    Ex1.6

---
+++ part1/unicafe/src/App.js
@@ -0,0 +1,37 @@
+import React, { useState } from 'react'
+
+const Button = ({onClick,text}) => <button onClick={onClick}>{text}</button>
+
+const Statistic = ({text, value}) => <>{text} {value} <br /></>
+
+
+const App = () => {
+  const [statistics, setStatistics] = useState({
+    good: 0,
+    neutral: 0,
+    bad: 0
+  }) 
+  
+
+  const clickGood = () => setStatistics({...statistics, good: statistics.good+1} )
+  const clickBad = () => setStatistics({...statistics, bad: statistics.bad+1} )
+  const clickNeutral= () => setStatistics({...statistics, neutral: statistics.neutral+1} )
+
+  return (
+    <div>
+      <h1>Give Feedback</h1>
+      <Button onClick={clickGood} text="Good"  />
+      <Button onClick={clickNeutral} text="Neutral"  />
+      <Button onClick={clickBad} text="Bad"  />
+      <br />
+      <h1>Statistics</h1>
+      <p>
+        <Statistic text="Good" value={statistics.good} />
+        <Statistic text="Neutral" value={statistics.neutral} />
+        <Statistic text="Bad" value={statistics.bad} />
+      </p>
+    </div>
+  )
+}
+
+export default App;
