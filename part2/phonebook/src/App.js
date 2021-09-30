ee7d129a8a9b8ab07755eb36f0cd77f080b2a2dd
Flavio Garrido <fbonnin@teceng-gaming.com.ar>
2021-09-30 09:26:02 -0300

    Ex2.7

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -9,11 +9,15 @@ const App = () => {
 
   const saveName=(event)=>{
     event.preventDefault()
-    const newPerson = {
-      name: newName
+    if(persons.find(person => person.name == newName)){
+      alert(`${newName} is already added to phonebook`)
+    } else{
+      const newPerson = {
+        name: newName
+      }
+      setPersons(persons.concat(newPerson))
+      setNewName('')
     }
-    setPersons(persons.concat(newPerson))
-    setNewName('')
   }
 
   return (
