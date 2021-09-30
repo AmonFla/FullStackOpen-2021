0f0e2c48cf163d8a56b9c2c61d22ba3f20de0130
Flavio Garrido <fbonnin@teceng-gaming.com.ar>
2021-09-30 09:19:52 -0300

    Ex2.6

---
+++ part2/phonebook/src/App.js
@@ -0,0 +1,38 @@
+import React, { useState } from 'react'
+import Person from './components/Person'
+
+const App = () => {
+  const [ persons, setPersons ] = useState([
+    { name: 'Arto Hellas' }
+  ]) 
+  const [ newName, setNewName ] = useState('')
+
+  const saveName=(event)=>{
+    event.preventDefault()
+    const newPerson = {
+      name: newName
+    }
+    setPersons(persons.concat(newPerson))
+    setNewName('')
+  }
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={saveName}>
+        <div>
+          name: <input 
+            onChange={(event)=>setNewName(event.target.value)} 
+            value={newName} />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      {persons.map((person)=><Person key={person.name}  name={person.name} />) }
+    </div>
+  )
+}
+
+export default App
