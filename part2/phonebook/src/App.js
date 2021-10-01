d1d91c0e2a4c9684310efc795bfd484ce8e67ad7
Flavio Garrido <fbonnin@teceng-gaming.com.ar>
2021-10-01 09:27:54 -0300

    Ex2.19

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -3,12 +3,14 @@ import servPerson from './services/persons'
 import Persons from './components/Persons'
 import Search from './components/Search'
 import NewPerson from './components/NewPerson'
+import Notification from './components/Notification'
 
 const App = () => {
   const [ persons, setPersons] = useState([])
   const [ newName, setNewName ] = useState('')
   const [ newNumber, setNewNumber ] = useState('')
   const [ filter, setFilter ] = useState('')
+  const [ notificationMessage, setNotificationMessage] = useState(null)
 
   useEffect(()=>{
     servPerson.getAll().then(persons => setPersons(persons))
@@ -16,8 +18,8 @@ const App = () => {
   
   const saveName=(event)=>{
     event.preventDefault()
-    const person = {...persons.find(person => person.name === newName)}
-    if(person){
+    const person = {...persons.find(person => person.name === newName)} 
+    if(Object.entries(person).length !== 0){
       if (window.confirm(`${newName} is already added to phonebook, replace the old number with a new one?`)){
          person.number = newNumber
          servPerson.updatePerson(person.id, person).then(resp =>
@@ -35,6 +37,7 @@ const App = () => {
           setPersons(persons.concat(response))
           setNewName('')
           setNewNumber('')
+          setNotificationMessage(`Added ${response.name}`)
         })
     }
   }
@@ -65,6 +68,7 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
+      <Notification message={notificationMessage} />
       <Search  onChange={(event)=>setFilter(event.target.value)}  value={filter} /> 
       <NewPerson onSubmit={saveName} name={name} number={number} />      
       <Persons personToShow={personToShow} deletePerson={deletePerson} />
