3459478dba06ed156f2705fb0176f5a7a82c2857
Flavio Garrido <fbonnin@teceng-gaming.com.ar>
2021-10-12 20:47:19 -0300

    Ex5.9

--- part5/blog/src/components/BlogList.js
+++ part5/blog/src/components/BlogList.js
@@ -5,14 +5,16 @@ import TogglableBlogItem from './TogglableBlogItem'
 import BlogDetail from './BlogDetail'
 
 const BlogList = ({ blogs, setBlogs }) =>
-  blogs.map(blog => {
-    return (
+  [].concat(blogs)
+    .sort((a, b) => a.likes < b.likes ? 1 : -1)
+    .map(blog => {
+      return (
             <div key={blog.id} className="blogStyle">
                 <BlogItem key={blog.id} blog={blog} />
                 <TogglableBlogItem key={blog.id} buttonShow='view' buttonHide="hide">
                     <BlogDetail key={blog.id} blog={blog} blogs={blogs} setBlogs={setBlogs} />
                 </TogglableBlogItem>
             </div>
-    )
-  })
+      )
+    })
 export default BlogList
